ba130e13c9c66757b0948c40a27af0b3323fcf23
joshuaoforisurg <joshuaoforidonkor@gmail.com>
2026-07-20 22:55:08 +0100

    feat: add surgeon preference Gold adapter

---
+++ platform/modules/operational/stock_inventory/integrations/__init__.py
@@ -0,0 +1,11 @@
+from integrations.surgeon_preference import (
+    PreferenceRequirementResolution,
+    load_preference_cards,
+    resolve_preference_requirements,
+)
+
+__all__ = [
+    "PreferenceRequirementResolution",
+    "load_preference_cards",
+    "resolve_preference_requirements",
+]
